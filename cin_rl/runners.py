ca8c200868c8e9f2ea17f63e7ab2b82719a51a11
Stelios Tymvios <solliet@protonmail.com>
2020-01-24 16:46:39 +0200

    Runner functions

---
+++ cin_rl/runners.py
@@ -0,0 +1,54 @@
+from collections import namedtuple
+import numpy as np
+import itertools
+
+Experience = namedtuple("Experience", ["obs", "action", "reward", "newobs", "done"])
+
+def episode_runner(envfn, f):
+    env = envfn()
+    while True:
+        history = []
+        done = False
+        obs = env.reset()
+        while not done:
+            action = f(obs)
+            nobs, reward, done, _ = env.step(action)
+            history.append(Experience(obs, action, reward, nobs, done))
+            obs = nobs
+        yield Experience(*map(np.array, zip(*history)))
+        del history[:]
+
+
+def nstep_runner(envfn, f, steps):
+    env = envfn()
+    history = []
+    done = False
+    obs = env.reset()
+    
+    obs_history = np.zeros((steps, *obs.shape))
+    nobs_history = np.zeros_like(obs_history)
+    reward_history = np.zeros(steps)
+    action_history = np.zeros(steps)
+    done_history = np.ones(steps)
+
+    while True:
+        for timestep in range(steps):
+            action = f(obs)
+            nobs, reward, done, _ = env.step(action)
+            action_history[timestep] = action
+            reward_history[timestep] = reward
+            done_history[timestep] = done
+            nobs_history[timestep] = nobs
+            obs_history[timestep] = obs
+            obs = nobs
+            if done:
+                obs = env.reset()
+                done = False
+                break
+        
+        yield Experience(obs_history, action_history, reward_history, nobs_history, done_history)
+        obs_history = np.zeros((steps, *obs.shape))
+        nobs_history = np.zeros_like(obs_history)
+        reward_history = np.zeros(steps)
+        action_history = np.zeros(steps)
+        done_history = np.ones(steps)
